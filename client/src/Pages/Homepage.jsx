e24adac0fbf79033bfacbcfdfb19e91a115dffa7
Abhinav <dhruvsahu550@gmail.com>
2025-05-02 00:28:14 +0530

    Added hotel cards and featured destination

--- client/src/Pages/Homepage.jsx
+++ client/src/Pages/Homepage.jsx
@@ -1,9 +1,11 @@
+import FeaturedDestination from "../components/FeaturedDestination";
 import Hero from "../components/Hero";
 
 export default function Homepage(){
     return (
         <>
             <Hero />
+            <FeaturedDestination />
         </>
     )
 }
