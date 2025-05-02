bfd808f949bdfcfd1d16c3c0a04aeb95099f3c82
Abhinav <dhruvsahu550@gmail.com>
2025-05-02 14:08:42 +0530

    Setup testimoniald newsletter section

--- client/src/Pages/Homepage.jsx
+++ client/src/Pages/Homepage.jsx
@@ -1,6 +1,8 @@
 import ExclusiveOffers from "../components/ExclusiveOffers";
 import FeaturedDestination from "../components/FeaturedDestination";
 import Hero from "../components/Hero";
+import NewsLetter from "../components/NewsLetter";
+import Testimonial from "../components/Testimonial";
 
 export default function Homepage(){
     return (
@@ -8,6 +10,8 @@ export default function Homepage(){
             <Hero />
             <FeaturedDestination />
             <ExclusiveOffers />
+            <Testimonial />
+            <NewsLetter />
         </>
     )
 }
